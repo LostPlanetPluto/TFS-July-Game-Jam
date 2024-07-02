6d1b0b1ef988fc85fb870d211d9ab6a592ee504f
rcuartero <106171055+rcuartero@users.noreply.github.com>
2024-07-02 13:10:37 -0400

    Initial Push

---
+++ Assets/Scripts/Player/Movement/Player_Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [Header("Movement Properties")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
+    private Rigidbody rb;
+
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb != null)
+        {
+            if (direction != Vector3.zero) rb.AddForce(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
